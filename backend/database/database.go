d82df8360d32512eccaae7cedd7a0fd9c355a364
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2024-08-22 14:43:40 -0300

    feat: iimplement database connection

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,16 +1,96 @@
 package database
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/charmbracelet/log"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
 	"tsukuyomi/config"
 )
 
-type Service struct {
-	tx         *gorm.Tx
-	connection *gorm.DB
+type Service interface {
+	BeginTransaction() *gorm.DB
+	Commit() error
+	Query() (*gorm.DB, error)
+	Rollback() error
 }
 
-func New(config config.Config) (*Service, error) {
-	return &Service{}, nil
+type service struct {
+	connection *gorm.DB
+	tx         *gorm.DB
+}
+
+func New(config *config.Config) Service {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=%s&collation=%s&parseTime=True&loc=Local",
+		config.Database.User,
+		config.Database.Pass,
+		config.Database.Host,
+		config.Database.Port,
+		config.Database.Schema,
+		config.Database.Charset,
+		config.Database.Collation,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &service{
+		connection: db,
+	}
+}
+
+func (s *service) BeginTransaction() *gorm.DB {
+	if s.tx != nil {
+		return s.tx
+	}
+
+	s.tx = s.connection.Begin()
+	return s.tx
+}
+
+func (s *service) Commit() error {
+	if s.tx == nil {
+		return errors.New("transaction not started")
+	}
+
+	defer func() {
+		s.tx = nil
+	}()
+
+	if err := s.tx.Commit(); err.Error != nil {
+		s.tx.Rollback()
+		return err.Error
+	}
+
+	return nil
+}
+
+func (s *service) Query() (*gorm.DB, error) {
+	if s.tx != nil {
+		return nil, errors.New("transaction in progress")
+	}
+
+	return s.connection, nil
+
+}
+
+func (s *service) Rollback() error {
+	if s.tx != nil {
+		return errors.New("transaction not started")
+	}
+
+	defer func() {
+		s.tx = nil
+	}()
+
+	if err := s.tx.Rollback(); err != nil {
+		return err.Error
+	}
+
+	return nil
 }
