1c51c2ecc1ab48e839e403c74883817b395a99e7
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2024-08-23 19:12:00 -0300

    feat: reflect database change and basic struct validation before creation

--- backend/models/empresa.go
+++ backend/models/empresa.go
@@ -4,16 +4,24 @@ import (
 	"time"
 
 	"github.com/charmbracelet/log"
+	"github.com/invopop/validation"
 	"gorm.io/gorm"
 )
 
 type Empresa struct {
 	ID         int            `gorm:"primaryKey" json:"id"`
-	Nome       string         `gorm:"column:nome;size:255" json:"nome"`
-	CNPJ       string         `gorm:"column:cnpj;size:20" json:"cnpj"`
+	Nome       string         `gorm:"column:nome;size:255;unique;not null" json:"nome"`
+	CNPJ       string         `gorm:"column:cnpj;size:20;unique;not null" json:"cnpj"`
 	Criado     time.Time      `gorm:"column:criado;autoCreateTime"`
 	Atualizado *time.Time     `gorm:"column:atualizado;autoUpdateTime"`
 	Apagado    gorm.DeletedAt `gorm:"column:apagado"`
+}
+
+func (e Empresa) BeforeCreate(tx *gorm.DB) error {
+	return validation.ValidateStruct(&e,
+		validation.Field(&e.Nome, validation.Required, validation.NilOrNotEmpty),
+		validation.Field(&e.CNPJ, validation.Required, validation.NilOrNotEmpty),
+	)
 }
 
 func (e *Empresa) AfterCreate(tx *gorm.DB) error {
--- backend/models/endereco.go
+++ backend/models/endereco.go
@@ -4,21 +4,33 @@ import (
 	"time"
 
 	"github.com/charmbracelet/log"
+	"github.com/invopop/validation"
 	"gorm.io/gorm"
 )
 
 type Endereco struct {
 	ID          int            `gorm:"primaryKey" json:"id"`
-	Logradouro  string         `gorm:"column:logradouro;size:255" json:"logradouro"`
-	Numero      int            `gorm:"column:numero" json:"numero"`
+	Logradouro  string         `gorm:"column:logradouro;size:255;unique;not null" json:"logradouro"`
+	Numero      string         `gorm:"column:numero;size:10;not null" json:"numero"`
 	Complemento *string        `gorm:"column:complemento;size:100" json:"complemento"`
-	Bairro      string         `gorm:"column:bairro;size:100" json:"bairro"`
-	Cidade      string         `gorm:"column:cidade;size:100" json:"cidade"`
-	CEP         string         `gorm:"column:cep;size:9" json:"cep"`
-	Estado      string         `gorm:"column:estado;size:20" json:"estado"`
+	Bairro      string         `gorm:"column:bairro;size:100;not null" json:"bairro"`
+	Cidade      string         `gorm:"column:cidade;size:100;not null" json:"cidade"`
+	CEP         string         `gorm:"column:cep;size:9;not null" json:"cep"`
+	Estado      string         `gorm:"column:estado;size:20;not null" json:"estado"`
 	Criado      time.Time      `gorm:"column:criado;autoCreateTime"`
 	Atualizado  *time.Time     `gorm:"column:atualizado;autoUpdateTime"`
 	Apagado     gorm.DeletedAt `gorm:"column:apagado"`
+}
+
+func (e Endereco) BeforeCreate(tx *gorm.DB) error {
+	return validation.ValidateStruct(&e,
+		validation.Field(&e.Logradouro, validation.Required, validation.NilOrNotEmpty),
+		validation.Field(&e.Numero, validation.Required, validation.NilOrNotEmpty),
+		validation.Field(&e.Bairro, validation.Required, validation.NilOrNotEmpty),
+		validation.Field(&e.Cidade, validation.Required, validation.NilOrNotEmpty),
+		validation.Field(&e.CEP, validation.Required, validation.NilOrNotEmpty),
+		validation.Field(&e.Estado, validation.Required, validation.NilOrNotEmpty),
+	)
 }
 
 func (e *Endereco) AfterCreate(tx *gorm.DB) error {
