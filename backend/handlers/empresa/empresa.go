3e61ef17c0aa2eb480b63322b8640f488073c205
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2024-09-09 17:35:35 -0300

    feat: implement all base functions in handlers

--- backend/handlers/empresa/empresa.go
+++ backend/handlers/empresa/empresa.go
@@ -1,10 +1,9 @@
 package empresa
 
 import (
-	"errors"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
 
 	"tsukuyomi/models"
 	empresaService "tsukuyomi/services/empresa"
@@ -12,13 +11,29 @@ import (
 
 type EmpresaHandler interface {
 	Create(c *fiber.Ctx) error
-	GetAll(c *fiber.Ctx) error
-	GetByID(c *fiber.Ctx) error
+	FindAll(c *fiber.Ctx) error
+	FindByID(c *fiber.Ctx) error
 	Update(c *fiber.Ctx) error
+	Delete(c *fiber.Ctx) error
 }
+
 type empresaHandler struct {
 	Service empresaService.Service
 }
+
+var (
+	ERROR_CREATE   = "Falha ao criar a empresa informada."
+	ERROR_FIND_ALL = "Falha ao consultar empresas."
+	ERROR_FIND_BY  = "Falha ao consultar empresa por ID."
+	ERROR_UPDATE   = "Falha ao atualizar empresa."
+	ERROR_DELETE   = "Falha ao apagar a empresa informado."
+
+	CREATE_SUCCESS   = "Empresa criada com sucesso."
+	FIND_ALL_SUCCESS = "Consulta realizada com sucesso."
+	FIND_BY_SUCCESS  = "Consulta realizada com sucesso."
+	UPDATE_SUCCESS   = "Empresa atualizada com sucesso."
+	DELETE_SUCCESS   = "Empresa apagada com sucesso."
+)
 
 func NewHandler(service empresaService.Service) EmpresaHandler {
 	return &empresaHandler{
@@ -26,86 +41,207 @@ func NewHandler(service empresaService.Service) EmpresaHandler {
 	}
 }
 
+// Create godoc
+// @Summary     Cadastra um nova empresa
+// @Description Cadastra um nova empresa de acordo com as informações fornecidas
+//
+// @Tags    Empresa
+// @Accept  json
+// @Produce json
+//
+// @Param nome body string true "Nome da empresa"
+// @Param cnpj body string true "CNPJ da empresa"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /empresa [post]
 func (h *empresaHandler) Create(c *fiber.Ctx) error {
-	empresa := new(models.Empresa)
+	empresa := models.Empresa{}
 
 	c.BodyParser(&empresa)
 
-	result, err := h.Service.Create(*empresa)
+	empresa.Criado = time.Now()
+
+	empresa, err := h.Service.Create(c.UserContext(), empresa)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_CREATE,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: 1},
-		Data: result,
+		Count:   1,
+		Message: CREATE_SUCCESS,
+		Data:    empresa,
 	})
 }
 
-func (h *empresaHandler) GetAll(c *fiber.Ctx) error {
-	criteria := map[string]interface{}{}
-	c.BodyParser(&criteria)
+// FindAll godoc
+// @Summary     Retorna todos as empresas
+// @Description Retorna todos as empresas que atendam aos critérios informados
+//
+// @Tags    Empresa
+// @Accept  json
+// @Produce json
+//
+// @Param search query string false "Campo aberto para pesquisa"
+// @Param nome   query string false "Nome da empresa"
+// @Param cnpj   query string false "CNPJ da empresa"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /empresa [get]
+func (h *empresaHandler) FindAll(c *fiber.Ctx) error {
+	search := c.Query("search", "")
+	nome := c.Query("nome", "")
+	cnpj := c.Query("cnpj", "")
 
-	result, err := h.Service.GetAll(criteria)
+	result, err := h.Service.FindAll(c.UserContext(), search, nome, cnpj)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_FIND_ALL,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: len(result)},
-		Data: result,
+		Count:   len(result),
+		Message: FIND_ALL_SUCCESS,
+		Data:    result,
 	})
 }
 
-func (h *empresaHandler) GetByID(c *fiber.Ctx) error {
-	id := c.Params("id")
-
-	result, err := h.Service.GetByID(id)
-	if err != nil {
-		if ok := errors.Is(err, gorm.ErrRecordNotFound); ok {
-			return c.Status(fiber.StatusNotFound).JSON(models.Response{
-				Meta: models.Meta{Count: 0},
-			})
-		}
+// FindByID godoc
+// @Summary     Consulta uma empresa por ID
+// @Description Retorna as informações de uma empresa de acordo com seu ID
+//
+// @Tags    Empresa
+// @Accept  json
+// @Produce json
+//
+// @Param id path string true "O ID da empresa para retornar"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /empresa/{id} [get]
+func (h *empresaHandler) FindByID(c *fiber.Ctx) error {
+	id := c.Params("id", "")
+	if id == "" {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_FIND_BY,
+			Errors:  []string{"Nenhum ID informado."},
+		})
+	}
+
+	result, err := h.Service.FindByID(c.UserContext(), id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_FIND_BY,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: 1},
-		Data: result,
+		Count:   1,
+		Message: FIND_BY_SUCCESS,
+		Data:    result,
 	})
 }
 
+// Update godoc
+// @Summary     Atualiza uma empresa
+// @Description Atualiza um registro de empresa de acordo com o ID e as informações informadas
+//
+// @Tags    Empresa
+// @Accept  json
+// @Produce json
+//
+// @Param id   path string true  "O ID da empresa a ser atualizada"
+// @Param nome body string false "Nome da empresa"
+// @Param cnpj body string false "CNPJ da empresa"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /empresa/{id} [put]
 func (h *empresaHandler) Update(c *fiber.Ctx) error {
-	empresa := new(models.Empresa)
-	c.BodyParser(&empresa)
-
-	if empresa.ID == 0 {
+	id := c.Params("id", "")
+	if id == "" {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: "id is required",
+			Message: ERROR_UPDATE,
+			Errors:  []string{"Nenhum ID informado."},
 		})
 	}
 
-	result, err := h.Service.Update(*empresa)
+	empresa, err := h.Service.FindByID(c.UserContext(), id)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_UPDATE,
+			Errors:  []string{err.Error()},
+		})
+	}
+
+	c.BodyParser(&empresa)
+
+	now := time.Now()
+	empresa.Atualizado = &now
+
+	err = h.Service.Update(c.UserContext(), empresa)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_UPDATE,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: 1},
-		Data: result,
+		Count:   1,
+		Message: UPDATE_SUCCESS,
+		Data:    empresa,
+	})
+}
+
+// Delete godoc
+// @Summary     Apaga uma empresa
+// @Description Realiza um soft-delete de uma empresa com base no ID informado
+//
+// @Tags    Empresa
+// @Accept  json
+// @Produce json
+//
+// @Param id path string true "O ID da empresa a ser apagada"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /empresa/{id} [delete]
+func (h *empresaHandler) Delete(c *fiber.Ctx) error {
+	id := c.Params("id", "")
+	if id == "" {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_DELETE,
+			Errors:  []string{"Nenhum ID informado."},
+		})
+	}
+
+	err := h.Service.Delete(c.UserContext(), id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_DELETE,
+			Errors:  []string{err.Error()},
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(models.Response{
+		Message: DELETE_SUCCESS,
 	})
 }
--- backend/handlers/endereco/endereco.go
+++ backend/handlers/endereco/endereco.go
@@ -1,10 +1,9 @@
 package endereco
 
 import (
-	"errors"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
 
 	"tsukuyomi/models"
 	enderecoService "tsukuyomi/services/endereco"
@@ -12,13 +11,29 @@ import (
 
 type EnderecoHandler interface {
 	Create(c *fiber.Ctx) error
-	GetAll(c *fiber.Ctx) error
-	GetByID(c *fiber.Ctx) error
+	FindAll(c *fiber.Ctx) error
+	FindByID(c *fiber.Ctx) error
 	Update(c *fiber.Ctx) error
+	Delete(c *fiber.Ctx) error
 }
+
 type enderecoHandler struct {
 	Service enderecoService.Service
 }
+
+var (
+	ERROR_CREATE   = "Falha ao criar o endereço informado."
+	ERROR_FIND_ALL = "Falha ao consultar endereços."
+	ERROR_FIND_BY  = "Falha ao consultar endereço."
+	ERROR_UPDATE   = "Falha ao atualizar endereço."
+	ERROR_DELETE   = "Falha ao apagar o endereço informado."
+
+	CREATE_SUCCESS   = "Endereço criado com sucesso."
+	FIND_ALL_SUCCESS = "Consulta realizada com sucesso."
+	FIND_BY_SUCCESS  = "Consulta realizada com sucesso."
+	UPDATE_SUCCESS   = "Endereço atualizado com sucesso."
+	DELETE_SUCCESS   = "Endereço apagado com sucesso."
+)
 
 func NewHandler(service enderecoService.Service) EnderecoHandler {
 	return &enderecoHandler{
@@ -26,86 +41,228 @@ func NewHandler(service enderecoService.Service) EnderecoHandler {
 	}
 }
 
+// Create godoc
+// @Summary     Cadastra um novo endereço
+// @Description Cadastra um novo endereço de acordo com as informações fornecidas
+//
+// @Tags    Endereco
+// @Accept  json
+// @Produce json
+//
+// @Param logradouro  body string true  "Logradouro do endereço"
+// @Param numero      body string true  "Número do endereço"
+// @Param complemento body string false "Complemento do endereço, caso exista"
+// @Param bairro      body string true  "Nome do bairro"
+// @Param cidade      body string true  "Nome da cidade"
+// @Param cep         body string true  "CEP"
+// @Param estado      body string true  "Estado"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /endereco [post]
 func (h *enderecoHandler) Create(c *fiber.Ctx) error {
-	endereco := new(models.Endereco)
+	endereco := models.Endereco{}
 
 	c.BodyParser(&endereco)
 
-	result, err := h.Service.Create(*endereco)
+	endereco.Criado = time.Now()
+
+	endereco, err := h.Service.Create(c.UserContext(), endereco)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_CREATE,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: 1},
-		Data: result,
+		Count:   1,
+		Message: CREATE_SUCCESS,
+		Data:    endereco,
 	})
 }
 
-func (h *enderecoHandler) GetAll(c *fiber.Ctx) error {
-	criteria := map[string]interface{}{}
-	c.BodyParser(&criteria)
+// FindAll godoc
+// @Summary     Retorna todos os endereços
+// @Description Retorna todos os endereços que atendam aos critérios informados
+//
+// @Tags    Endereco
+// @Accept  json
+// @Produce json
+//
+// @Param search      query string false "Campo aberto para pesquisa"
+// @Param logradouro  query string false "Logradouro do endereço"
+// @Param numero      query string false "Número do endereço"
+// @Param complemento query string false "Complemento do endereço, caso exista"
+// @Param bairro      query string false "Nome do bairro"
+// @Param cidade      query string false "Nome da cidade"
+// @Param cep         query string false "CEP"
+// @Param estado      query string false "Estado"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /endereco [get]
+func (h *enderecoHandler) FindAll(c *fiber.Ctx) error {
+	search := c.Query("search", "")
+	logradouro := c.Query("logradouro", "")
+	numero := c.Query("numero", "")
+	complemento := c.Query("complemento", "")
+	bairro := c.Query("bairro", "")
+	cidade := c.Query("cidade", "")
+	cep := c.Query("cep", "")
+	estado := c.Query("estado", "")
 
-	result, err := h.Service.GetAll(criteria)
+	result, err := h.Service.FindAll(c.UserContext(), search, logradouro, numero, complemento, bairro, cidade, cep, estado)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_FIND_ALL,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: len(result)},
-		Data: result,
+		Count:   len(result),
+		Message: FIND_ALL_SUCCESS,
+		Data:    result,
 	})
 }
 
-func (h *enderecoHandler) GetByID(c *fiber.Ctx) error {
-	id := c.Params("id")
-
-	result, err := h.Service.GetByID(id)
-	if err != nil {
-		if ok := errors.Is(err, gorm.ErrRecordNotFound); ok {
-			return c.Status(fiber.StatusNotFound).JSON(models.Response{
-				Meta: models.Meta{Count: 0},
-			})
-		}
+// FindByID godoc
+// @Summary     Consulta um endereço por ID
+// @Description Retorna as informações de um endereço de acordo com seu ID
+//
+// @Tags    Endereco
+// @Accept  json
+// @Produce json
+//
+// @Param id path string true  "O ID do endereço para retornar"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /endereco/{id} [get]
+func (h *enderecoHandler) FindByID(c *fiber.Ctx) error {
+	id := c.Params("id", "")
+	if id == "" {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_UPDATE,
+			Errors:  []string{"Nenhum ID informado."},
+		})
+	}
+
+	result, err := h.Service.FindByID(c.UserContext(), id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_FIND_BY,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: 1},
-		Data: result,
+		Count:   1,
+		Message: FIND_BY_SUCCESS,
+		Data:    result,
 	})
+
 }
 
+// Update godoc
+// @Summary     Atualiza um endereço
+// @Description Atualiza um registro de endereço de acordo com o ID e as informações informadas
+//
+// @Tags    Endereco
+// @Accept  json
+// @Produce json
+//
+// @Param id          path string true  "O ID do endereço a ser atualizado"
+// @Param logradouro  body string false "Logradouro do endereço"
+// @Param numero      body string false "Número do endereço"
+// @Param complemento body string false "Complemento do endereço, caso exista"
+// @Param bairro      body string false "Nome do bairro"
+// @Param cidade      body string false "Nome da cidade"
+// @Param cep         body string false "CEP"
+// @Param estado      body string false "Estado"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /endereco/{id} [put]
 func (h *enderecoHandler) Update(c *fiber.Ctx) error {
-	endereco := new(models.Endereco)
-	c.BodyParser(&endereco)
-
-	if endereco.ID == 0 {
+	id := c.Params("id", "")
+	if id == "" {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: "id is required",
+			Message: ERROR_UPDATE,
+			Errors:  []string{"Nenhum ID informado."},
 		})
 	}
 
-	result, err := h.Service.Update(*endereco)
+	endereco, err := h.Service.FindByID(c.UserContext(), id)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
-			Meta:  models.Meta{Count: 0},
-			Error: err.Error(),
+			Message: ERROR_UPDATE,
+			Errors:  []string{err.Error()},
+		})
+	}
+
+	c.BodyParser(&endereco)
+
+	now := time.Now()
+	endereco.Atualizado = &now
+
+	err = h.Service.Update(c.UserContext(), endereco)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_UPDATE,
+			Errors:  []string{err.Error()},
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(models.Response{
-		Meta: models.Meta{Count: 1},
-		Data: result,
+		Count:   1,
+		Message: UPDATE_SUCCESS,
+		Data:    endereco,
+	})
+}
+
+// Delete godoc
+// @Summary     Apaga um endereço
+// @Description Realiza um soft-delete de um endereço com base no ID informado
+//
+// @Tags    Endereco
+// @Accept  json
+// @Produce json
+//
+// @Param id path string true "O ID do endereço a ser apagado"
+//
+// @Success 200 {object} models.Response
+// @Failure 400 {object} models.Response
+// @Failure 500 {object} models.Response
+//
+// @Router /endereco/{id} [delete]
+func (h *enderecoHandler) Delete(c *fiber.Ctx) error {
+	id := c.Params("id", "")
+	if id == "" {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_DELETE,
+			Errors:  []string{"Nenhum ID informado."},
+		})
+	}
+
+	err := h.Service.Delete(c.UserContext(), id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(models.Response{
+			Message: ERROR_DELETE,
+			Errors:  []string{err.Error()},
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(models.Response{
+		Message: DELETE_SUCCESS,
 	})
 }
