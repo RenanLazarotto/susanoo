2849d10d13ce02156b2274811c54c8de5b05d638
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2024-09-10 15:03:24 -0300

    fix: add missing Empresa entry in Emprego struct

--- backend/models/emprego.go
+++ backend/models/emprego.go
@@ -4,7 +4,8 @@ import "time"
 
 type Emprego struct {
 	ID                 int64      `json:"id"`
-	IDEmpresa          int64      `json:"id_empresa"`
+	IDEmpresa          int64      `json:"id_empresa,omitempty"`
+	Empresa            Empresa    `json:"empresa,omitempty"`
 	Ocupacao           string     `json:"ocupacao"`
 	RemuneracaoInicial float64    `json:"remuneracao_inicial"`
 	TipoContrato       string     `json:"tipo_contrato"`
