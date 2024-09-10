9a960124fcc42edeb29b4ac9380e22b1cb4787a8
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2024-09-10 14:18:44 -0300

    feat: emprego model

---
+++ backend/models/emprego.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Emprego struct {
+	ID                 int64      `json:"id"`
+	IDEmpresa          int64      `json:"id_empresa"`
+	Ocupacao           string     `json:"ocupacao"`
+	RemuneracaoInicial float64    `json:"remuneracao_inicial"`
+	TipoContrato       string     `json:"tipo_contrato"`
+	DataInicio         time.Time  `json:"data_inicio"`
+	DataFim            *time.Time `json:"data_fim"`
+	CargaHoraria       int64      `json:"carga_horaria"`
+	Criado             time.Time  `json:"criado"`
+	Atualizado         *time.Time `json:"atualizado"`
+	Apagado            *time.Time `json:"apagado"`
+}
