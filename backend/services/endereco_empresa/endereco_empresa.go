6f05af5aabb1174f1bc43f3e18262b2d5e1ca9b7
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2024-09-10 18:19:46 -0300

    feat: add methods to get Endereco and Empresa in endereco_empresa service

--- backend/services/endereco_empresa/endereco_empresa.go
+++ backend/services/endereco_empresa/endereco_empresa.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"tsukuyomi/models"
+	"tsukuyomi/repositories/empresa"
+	"tsukuyomi/repositories/endereco"
 	enderecoempresa "tsukuyomi/repositories/endereco_empresa"
 )
 
@@ -11,15 +13,21 @@ type Service interface {
 	Assign(ctx context.Context, empresa models.Empresa, endereco models.Endereco) (models.EnderecoEmpresa, error)
 	GetEmpresasByEndereco(ctx context.Context, id_endereco string) ([]models.Empresa, error)
 	GetEnderecosByEmpresa(ctx context.Context, id_empresa string) ([]models.Endereco, error)
+	GetEmpresaByID(ctx context.Context, id_empresa string) (models.Empresa, error)
+	GetEnderecoByID(ctx context.Context, id_endereco string) (models.Endereco, error)
 }
 
 type service struct {
-	repository enderecoempresa.Repository
+	repository         enderecoempresa.Repository
+	EmpresaRepository  empresa.Repository
+	EnderecoRepository endereco.Repository
 }
 
-func NewService(repository enderecoempresa.Repository) Service {
+func NewService(repository enderecoempresa.Repository, empresaRepository empresa.Repository, enderecoRepository endereco.Repository) Service {
 	return &service{
-		repository: repository,
+		repository:         repository,
+		EmpresaRepository:  empresaRepository,
+		EnderecoRepository: enderecoRepository,
 	}
 }
 
@@ -33,4 +41,12 @@ func (s *service) GetEmpresasByEndereco(ctx context.Context, id_endereco string)
 
 func (s *service) GetEnderecosByEmpresa(ctx context.Context, id_empresa string) ([]models.Endereco, error) {
 	return s.repository.GetEnderecosByEmpresa(ctx, id_empresa)
+}
+
+func (s *service) GetEmpresaByID(ctx context.Context, id_empresa string) (models.Empresa, error) {
+	return s.EmpresaRepository.FindByID(ctx, id_empresa)
+}
+
+func (s *service) GetEnderecoByID(ctx context.Context, id_endereco string) (models.Endereco, error) {
+	return s.EnderecoRepository.FindByID(ctx, id_endereco)
 }
